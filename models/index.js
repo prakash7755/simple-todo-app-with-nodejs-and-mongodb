59df0db1a01b3c18038468f69406fa9d72841ba2
prakash <mailmeprakash1992@gmail.com>
2017-12-22 14:38:46 +0530

    bcrype added and tocken get

--- models/index.js
+++ models/index.js
@@ -2,11 +2,13 @@
 
 const db = require('./db');
 const Todo = require('./todo');
+const User = require('./user');
 
 
 
 module.exports = { 
 	db,
-	Todo
+	Todo,
+	User
  };
  
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 'use strict';
 const todoRouter = require('./todo');
+const userAuth = require('./user-auth')
 module.exports = app => {
 
 	app.use('/api/todo', todoRouter);
+	app.use('/api/auth', userAuth);
 
 };
