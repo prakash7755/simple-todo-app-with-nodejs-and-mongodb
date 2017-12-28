7cb49f24db31f2f5f22b68ced5e9229bd1b2088a
murali jaya <muralijaya2011@gmail.com>
2017-12-29 01:17:07 +0530

    login page

--- config/index.js
+++ config/index.js
@@ -1,8 +1,8 @@
 'use strict';
 const passportSetUp = require('../config');
 const passport = require('passport');
-// const GoogleStrategy = require('passport-google-oauth20');
-const GoogleStrategy = require('passport-google').Strategy;
+const GoogleStrategy = require('passport-google-oauth20');
+// const GoogleStrategy = require('passport-google').Strategy;
 const FacebookStrategy = require('passport-facebook').Strategy;
 const keys = require('./keys');
 const { User } = require('../models/');
@@ -11,27 +11,20 @@ const jwt = require('jsonwebtoken');
 
 
 
-// passport.use(new GoogleStrategy({
-//   callbackURL: "/auth/google/login",
-//   clientID: keys.google.clientID,
-//   clientSecret: keys.google.clientSecret,
-//   profileFields: ['id', 'displayName', 'gender', 'email']
-// }, (accessToken, refreshToken, profile, done) => {
-//  console.log(profile.displayName)
-//  console.log(profile.id)
-// })
-// );
-
-
 passport.use(new GoogleStrategy({
-    returnURL: 'http://localhost:3000/auth/google/login',
-    realm: 'http://localhost:3000/'
-  },
-  function(identifier, profile, done) {
-    console.log(profile)
-    done(null, profile);
-}
-));
+  callbackURL: "/auth/google/login",
+  clientID: keys.google.clientID,
+  clientSecret: keys.google.clientSecret,
+  profileFields: ['id', 'displayName', 'gender', 'email']
+}, (accessToken, refreshToken, profile, done) => {
+ const token = jwt.sign({ accessToken }, 'token');
+ console.log(profile)
+ done({profileId: profile.id,
+            Name: profile.displayName,
+           token: accessToken});
+})
+);
+
 
 
 passport.use(new FacebookStrategy({
--- routes/google-user.js
+++ routes/google-user.js
@@ -3,23 +3,16 @@
 const router = require('express').Router();
 const passport = require('passport');
 
+   /*
+    * Google Login User Verification
+    */
 
-// router.get('/google',  passport.authenticate('google', { scope : ['profile']}));
-
-// router.get('/google/login', passport.authenticate('google', { failureRedirect: '/login' }), 
-// 	(req, res) => {
-//               res.json({message: "success"});
-
-// });
-
-router.get('/google',
-    passport.authenticate('google', { scope: 'email' })
-);
+router.get('/google',  passport.authenticate('google', { scope : ['profile']}));
 
 router.get('/google/login', (req, res, next) => {
    const google = passport.authenticate('google', { scope: 'email' }, (err, user) => {
         if (user) {
-            res.send(user)
+            res.send(user.email)
         }
         if (err) {
             res.send(err)
@@ -28,6 +21,12 @@ router.get('/google/login', (req, res, next) => {
 google(req, res, next);
 });
 
+
+  /*
+   * Facebook Login User Verification
+   */
+
+
 router.get('/fb',
     passport.authenticate('facebook', { scope: 'email' })
 );
@@ -35,7 +34,10 @@ router.get('/fb',
 router.get('/fb/callback', (req, res, next) => {
     const fb = passport.authenticate('facebook', { scope: 'email' }, (err, user) => {
         if (user) {
-            res.send(user)
+            var token = jwt.sign({ password }, 'token');
+             return res.json({token})
+
+            // res.send(user)
         }
         if (err) {
             res.send(err)
